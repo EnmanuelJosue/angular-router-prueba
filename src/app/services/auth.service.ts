9f40276251972eb3f053e6b17183d5f5c9028c03
ENMANUEL <enmanueljosue2612@hotmail.com>
2022-01-18 22:20:12 -0400

    first commit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,34 +6,36 @@ import { environment } from './../../environments/environment';
 import { Auth } from './../models/auth.model';
 import { User } from './../models/user.model';
 import { TokenService } from './../services/token.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private apiUrl = `${environment.API_URL}/api/auth`;
+  private user = new BehaviorSubject<User | null>(null);
 
-  constructor(
-    private http: HttpClient,
-    private tokenService: TokenService
-  ) { }
+  user$ = this.user.asObservable();
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
   login(email: string, password: string) {
-    return this.http.post<Auth>(`${this.apiUrl}/login`, {email, password})
-    .pipe(
-      tap(response => this.tokenService.saveToken(response.access_token))
-    );
+    return this.http
+      .post<Auth>(`${this.apiUrl}/login`, { email, password })
+      .pipe(
+        tap((response) => this.tokenService.saveToken(response.access_token))
+      );
   }
 
   getProfile() {
-    return this.http.get<User>(`${this.apiUrl}/profile`);
+    return this.http
+      .get<User>(`${this.apiUrl}/profile`)
+      .pipe(tap((user) => this.user.next(user)));
   }
 
   loginAndGet(email: string, password: string) {
-    return this.login(email, password)
-    .pipe(
-      switchMap(() => this.getProfile()),
-    )
+    return this.login(email, password).pipe(switchMap(() => this.getProfile()));
+  }
+  logout() {
+    this.tokenService.removeToken();
   }
 }
